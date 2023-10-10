da5f774201650cc4ac248bffb6309ecc1532de32
Norbert00 <norbert.jurek@protonmail.com>
2023-10-10 12:36:50 +0200

    Implemented logic with game

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+
+from paddle import Paddle
+from game_screen import GameScreen
+
+LEFT_PADDLE = (-370, 0)
+RIGHT_PADDLE = (370, 0)
+
+right_paddle = Paddle(RIGHT_PADDLE)
+left_paddle = Paddle(LEFT_PADDLE)
+
+screen = GameScreen()
+screen.move_paddles(right_paddle, "right")
+screen.move_paddles(left_paddle, "left")
+time.sleep(0.1)
+
+game_is_on = True
+while game_is_on:
+    screen.update_screen()
