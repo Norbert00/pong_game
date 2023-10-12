c67ba9448843d87a772a9189889f602496dea344
Norbert00 <norbert.jurek@protonmail.com>
2023-10-12 21:43:44 +0200

    Implemented bouncing of the ball

--- ball.py
+++ ball.py
@@ -14,16 +14,18 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=WIDTH, stretch_len=HEIGHT)
         self.color(BALL_COLOR)
         self.penup()
-
-
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
         time.sleep(0.1)
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-        if self.ycor() == 280:
-            print("hit the wall")
-            new_y -= 30
-            self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
 
