47132c4e9ab9c77ed056d8f47bc25507666d87c9
Norbert00 <norbert.jurek@protonmail.com>
2023-10-14 20:07:07 +0200

    Methods related to counting the score and resetting the position of the ball have been invoked

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from ball import Ball
 from paddle import Paddle
 from game_screen import GameScreen
+from scoreboard import Scoreboard
 
 LEFT_PADDLE = (-370, 0)
 RIGHT_PADDLE = (370, 0)
@@ -12,20 +13,29 @@ left_paddle = Paddle(LEFT_PADDLE)
 screen = GameScreen()
 screen.move_paddles(right_paddle, "right")
 screen.move_paddles(left_paddle, "left")
-time.sleep(0.1)
+
 
 ball = Ball()
+scoreboard = Scoreboard()
+
+
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.ball_speed)
     screen.update_screen()
     ball.move()
     if ball.ycor() > screen.top_wall() or ball.ycor() < screen.bottom_wall():
         ball.bounce_y()
     if ball.distance(right_paddle) < 50 and ball.xcor() > 340 or ball.distance(left_paddle) < 50 and ball.xcor() < -340:
         ball.bounce_x()
-    if ball.xcor() > 340:
+    if ball.xcor() > 400:
         ball.reset_position()
-    if ball.xcor() < - 340:
+        scoreboard.l_point()
+        scoreboard.update_scoreboard()
+    if ball.xcor() < - 400:
         ball.reset_position()
+        scoreboard.r_point()
+        scoreboard.update_scoreboard()
+
 
